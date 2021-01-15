29f2da8c28c082a9d74f0cb765e98136401a6e58
abhisekrai88 <kesihba.iar@gmail.com>
2021-01-15 20:54:04 +0800

    minor edits

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -30,14 +30,17 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = ['myapi.apps.MyapiConfig',
+INSTALLED_APPS = [
+    'myapi.apps.MyapiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Third-Party Apps
     'rest_framework',
+    
 ]
 
 MIDDLEWARE = [
